be4568815a304cd192ef2a5bb167ad8d7973d32f
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-05-01 18:01:50 +0200

    stage 2 brute force

--- hack.py
+++ hack.py
@@ -1,26 +1,49 @@
 from sys import argv
 from  socket import socket
-
-
+from string import ascii_lowercase
+from itertools import product
+from functools import reduce
+from operator import add
 
 # Create a new socket.
 # Connect to a host and a port using the socket.
-# Send a message from the third command line argument to the host using the socket.
-# Receive the server’s response.
-# Print the server’s response.
+# Tries different passwords until it finds the correct one.
+# Prints the password it found. when the server response is 'Connection success!"
 # Close the socket.
 
-domain,port,message = argv[1:]
 
+domain,port = argv[1:]
 address =( domain, int(port))
+
+def tuple_to_string(word):
+    return reduce(add,word,"")
+
+def find_password():
+    """
+     generate all passwords with lowercase letters and numbers 0 to 9
+    """
+    letters = ascii_lowercase + "0123456789"
+    generator = letters
+    while True:
+        for word in generator:
+            yield tuple_to_string(word)
+        generator = product(letters,generator)
+
+
 
 
 with socket() as  client:
     client.connect(address)
-    client.send(message.encode())
     size_buffer = 1024
-    response = client.recv(size_buffer)
-    print(response.decode())
+    for password in find_password():
+        client.send(password.encode())
+        response = client.recv(size_buffer).decode()
+        if response == "Connection success!":
+            print(password)
+            break
+
+
+
 
 
 
